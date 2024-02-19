16aaea443142bcb4018d1cd6b0237abd74853a39
Raifikie <leonard_weiss@web.dem>
2024-02-19 12:26:55 +0100

    responsive design: mobile device header + overlay

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,16 +1,23 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
   activeTab: string = '';
+  mobileMenuState: 'show' | 'hide' | 'transition' = 'hide';
 
   setActiveTab(tab:string){
     this.activeTab = tab;
+  }
+
+  mobileMenu(state: 'show' | 'hide' | 'transition'){
+    this.mobileMenuState = 'transition';
+    setTimeout(()=>this.mobileMenuState = state,150);
   }
 }
