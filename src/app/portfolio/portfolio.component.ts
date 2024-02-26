7c81f54be54e8ef9c3cd46be8f04a5e3d5fa90c2
Raifikie <leonard_weiss@web.dem>
2024-02-26 15:30:49 +0100

    add about me description

--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -14,7 +14,7 @@ export class PortfolioComponent {
     projects: PortfolioProject[] = [
       {
         name: 'Pokedex',
-        imgPath: 'assets/images/projects/Pokedex_test.png',
+        imgPath: 'assets/images/projects/Pokedex_Vorlage_Laptop.png',
         technologies: [
           'Javascript',
           'HTML',
@@ -22,12 +22,12 @@ export class PortfolioComponent {
           'Api'
         ],
         description:  'Based on the PokéAPI a simple library that provides and catalogues pokemon information.',
-        urlGit: '',
-        urlDemo: ''
+        urlGit: 'https://github.com/Raifiki/Pokedex',
+        urlDemo: 'https://leonard-weiss.developerakademie.net/Projekte/M09_Pokedex/index.html'
       },
       {
         name: 'Join',
-        imgPath: 'assets/images/projects/Join_test.png',
+        imgPath: 'assets/images/projects/Join_Vorlage_Laptop.png',
         technologies: [
           'Javascript',
           'HTML',
@@ -35,11 +35,11 @@ export class PortfolioComponent {
         ],
         description:  'Task manager inspired by the Kanban System. Create and organize tasks using drag and drop functions, assign users and categories. ',
         urlGit: 'https://github.com/Raifiki/Join_Portfolio',
-        urlDemo: ''
+        urlDemo: 'https://leonard-weiss.developerakademie.net/Projekte/M12_JoinPortfolio/index.html'
       },
       {
         name: 'Sharkie',
-        imgPath: 'assets/images/projects/Sharkie_test.png',
+        imgPath: 'assets/images/projects/Sharkie_Vorlage_Laptop.png',
         technologies: [
           'Javascript',
           'HTML',
@@ -48,21 +48,20 @@ export class PortfolioComponent {
         ],
         description:  'A simple Jump-and-Run game based on an object-oriented approach. Help sharkie to find coins and poison bottles to fight against the killer whale.',
         urlGit: 'https://github.com/Raifiki/gameSharky',
-        urlDemo: ''
+        urlDemo: 'https://leonard-weiss.developerakademie.net/Projekte/M11_Sharkie/index.html'
       },
       {
-        name: 'Ring of Fire',
-        imgPath: 'assets/images/projects/Sharkie_test.png',
+        name: 'This Page',
+        imgPath: 'assets/images/projects/Portfolio_Vorlage_Laptop.png',
         technologies: [
           'Angular',
           'Typscript',
           'HTML',
-          'CSS',
-          'Firebase'
+          'CSS'
         ],
-        description:  'A simple Jump-and-Run game based on an object-oriented approach. Help sharkie to find coins and poison bottles to fight against the killer whale.',
-        urlGit: '',
-        urlDemo: ''
+        description:  'My portfolio page where I introduce myself and present my best projects. Additionaly you can contact me trough a contact form. I am exicted to hear from you.',
+        urlGit: 'https://github.com/Raifiki/Portfolio',
+        urlDemo: 'https://leonard-weiss.developerakademie.net/angular-projects/M15_portfolio/'
       }
   ]
 }
